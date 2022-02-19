13e03a254f52e8e5b2e96b18ddd1f03c95fbc049
Mateo Estrada <mateoej12@gmail.com>
2022-02-19 13:58:24 -0800

    Working on the sqlite database setup.

--- lib/widgets/journal_entry_form.dart
+++ lib/widgets/journal_entry_form.dart
@@ -22,95 +22,114 @@ class _JournalEntryFormState extends State<JournalEntryForm> {
   // Form Key
   final formKey = GlobalKey<FormState>();
   // Instance of DTO
-  final journalEntryFieldsInstanceBucket = JournalEntryFields();
+  final journalEntryValue = JournalEntryFields();
+  // Instance of the database
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10),
-      child: Form(
-        key: formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              autofocus: true,
-              decoration: const InputDecoration(
-                  labelText: 'Title', border: OutlineInputBorder()),
-              onSaved: (value) {
-                // store value in an object
-                journalEntryFieldsInstanceBucket.title = value;
-                // Save in a database here
-              },
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please Enter a Title';
-                } else {
-                  return null;
-                }
-              },
-            ),
-            TextFormField(
-              autofocus: true,
-              decoration: const InputDecoration(
-                  labelText: 'Body', border: OutlineInputBorder()),
-              onSaved: (value) {
-                // store value in an object
-                journalEntryFieldsInstanceBucket.title = value;
-                // Save in a database here
-              },
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please Enter a Body';
-                } else {
-                  return null;
-                }
-              },
-            ),
-            TextFormField(
-              autofocus: true,
-              decoration: const InputDecoration(
-                  labelText: 'Datetime', border: OutlineInputBorder()),
-              onSaved: (value) {
-                // store value in an object
-                journalEntryFieldsInstanceBucket.title = value;
-                // Save in a database here
-              },
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please Enter a Datetime';
-                } else {
-                  return null;
-                }
-              },
-            ),
-            TextFormField(
-              autofocus: true,
-              decoration: const InputDecoration(
-                  labelText: 'Rating', border: OutlineInputBorder()),
-              onSaved: (value) {
-                // store value in an object
-                journalEntryFieldsInstanceBucket.title = value;
-                // Save in a database here
-              },
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please Enter a Rating';
-                } else {
-                  return null;
-                }
-              },
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-                onPressed: () {
-                  if (formKey.currentState!.validate()) {
-                    formKey.currentState?.save();
-                  }
-                },
-                child: const Text('Save Entry'))
-          ],
-        ),
+    return Padding(padding: const EdgeInsets.all(10), child: formContent());
+  }
+
+  Widget formContent() {
+    return Form(
+      key: formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          titleTextField(),
+          bodyTextField(),
+          ratingDropDown(),
+          const SizedBox(height: 10),
+          saveButton(),
+          cancelButton()
+        ],
       ),
     );
+  }
+
+  Widget titleTextField() {
+    return TextFormField(
+      autofocus: true,
+      decoration: const InputDecoration(
+          labelText: 'Title', border: OutlineInputBorder()),
+      onSaved: (value) {
+        // store value in an object
+        journalEntryValue.title = value;
+        // Save in a database here
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Please Enter a Title';
+        } else {
+          return null;
+        }
+      },
+    );
+  }
+
+  Widget bodyTextField() {
+    return TextFormField(
+      autofocus: true,
+      decoration: const InputDecoration(
+          labelText: 'Body', border: OutlineInputBorder()),
+      onSaved: (value) {
+        // store value in an object
+        journalEntryValue.title = value;
+        // Save in a database here
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Please Enter a Body';
+        } else {
+          return null;
+        }
+      },
+    );
+  }
+
+  Widget ratingDropDown() {
+    return TextFormField(
+      autofocus: true,
+      decoration: const InputDecoration(
+          labelText: 'Rating', border: OutlineInputBorder()),
+      onSaved: (value) {
+        // store value in an object
+        journalEntryValue.title = value;
+        // Save in a database here
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Please Enter a Rating';
+        } else {
+          return null;
+        }
+      },
+    );
+  }
+
+  Widget saveButton() {
+    return ElevatedButton(
+      child: const Text('Save'),
+      onPressed: () {
+        if (formKey.currentState!.validate()) {
+          formKey.currentState?.save();
+          print('SAVED TO DATABASE');
+          addDateToJournalEntryValue();
+        }
+      },
+    );
+  }
+
+  Widget cancelButton() {
+    return ElevatedButton(
+      child: const Text('Cancel'),
+      onPressed: () {
+        print('Cancelled!');
+      },
+    );
+  }
+
+  void addDateToJournalEntryValue() {
+    journalEntryValue.dateTime = DateTime.now();
   }
 }
